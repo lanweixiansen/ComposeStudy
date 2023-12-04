741314f3afe85f7fc3278493409e5776dd365a07
lanwe <lanweixiansen@outlook.com>
2023-12-04 16:46:41 +0800

    feat:.

--- libHome/src/main/java/com/example/libHome/HomeFragment.kt
+++ libHome/src/main/java/com/example/libHome/HomeFragment.kt
@@ -30,7 +30,6 @@ class HomeFragment : BaseFragment<HomeFragmentHomeBinding>() {
             delay(1000)
             mBinding.lottieView.playAnimation()
         }
-        test
     }
 
     override fun initDate() {
